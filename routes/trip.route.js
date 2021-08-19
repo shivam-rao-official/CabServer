6fa1ef10a3d26cef36adbf51b0998b6d86257fa5
shivam-rao-official <m.shivamrao@gmail.com>
2021-08-19 13:08:39 +0530

    added empty array in the return

--- routes/trip.route.js
+++ routes/trip.route.js
@@ -50,7 +50,7 @@ router.get('/viewAllTrips', async (req, res) => {
         if(data.length == 0) {
             return res.json({
                 status: true,
-                msg: `No data in the DB or The EmpID is wrong`
+                msg: []
             })
         }
         // if(data.empId == empId){
