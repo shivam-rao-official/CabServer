bb29251a01600885b211306395e6bf2682962c14
shivam-rao-official <m.shivamrao@gmail.com>
2021-08-24 12:36:13 +0530

    Changes made in the viewTrips route

--- routes/trip.route.js
+++ routes/trip.route.js
@@ -50,7 +50,7 @@ router.get('/viewAllTrips', async (req, res) => {
         if(data.length == 0) {
             return res.json({
                 status: false,
-                msg: null
+                msg: "No Data"
             })
         }
         // if(data.empId == empId){
